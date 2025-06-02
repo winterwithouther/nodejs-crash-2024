30b28717a36b8b139bd468122971a0d21c8c690b
Eman <egurung1117@gmail.com>
2025-06-02 12:24:12 -0400

    routing users by id

---
+++ server2.js
@@ -0,0 +1,31 @@
+import { createServer } from 'http'
+const PORT = process.env.PORT
+
+const users = [
+    {id: 1, name: "John Doe"},
+    {id: 2, name: "Jane Doe"},
+    {id: 3, name: "Jim Doe"},
+]
+
+const server = createServer((req, res) => {
+    if (req.url === '/api/users' && req.method === "GET") {
+        res.setHeader('Content-type', 'application/json')
+        res.write(JSON.stringify(users))
+        res.end()
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+        const id = req.url.split('/')[3]
+        console.log(id)
+
+        res.setHeader('Content-type', 'application/json')
+        res.write(JSON.stringify({id: 1, name: "John Dope"}))
+        res.end()
+    } else {
+        res.setHeader('Content-type', 'application/json')
+        res.write(JSON.stringify({message: "Route not found"}))
+        res.end()
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
