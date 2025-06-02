102106f58eba25ddfc3e7c571f05cf58927f484d
Eman <egurung1117@gmail.com>
2025-06-01 23:38:58 -0400

    rendering html files with js modules

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-import http from "http"
+import http from 'http'
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path'
 
-const PORT = 8000
+const PORT = process.env.PORT
 
-const server = http.createServer((req, res) => {
-    res.write('Hello world')
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-    res.end()
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.method == "GET") {
+            let filePath
+            if (req.url == "/") {
+                filePath = path.join(__dirname, "public", "index.html")
+            } else if (req.url == "/about") {
+                filePath = path.join(__dirname, "public", "about.html")
+            } else {
+                throw new Error('Not found')
+            }
+
+            const data = await fs.readFile(filePath)
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data)
+            res.end()
+        } else {
+            throw new Error("Method not allowed")
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'})
+        res.end('Server Error')
+    }
 })
 
 server.listen(PORT, () => {
