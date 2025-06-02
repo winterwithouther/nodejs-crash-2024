76ff25968ad872c6bbe8278705bc1213eb25e889
Eman <egurung1117@gmail.com>
2025-06-02 13:40:41 -0400

    adding middleware

--- server2.js
+++ server2.js
@@ -7,23 +7,39 @@ const users = [
     {id: 3, name: "Jim Doe"},
 ]
 
-const server = createServer((req, res) => {
-    if (req.url === '/api/users' && req.method === "GET") {
-        res.setHeader('Content-type', 'application/json')
-        res.write(JSON.stringify(users))
-        res.end()
-    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-        const id = req.url.split('/')[3]
-        console.log(id)
+// Logger middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+}
 
-        res.setHeader('Content-type', 'application/json')
-        res.write(JSON.stringify({id: 1, name: "John Dope"}))
-        res.end()
-    } else {
-        res.setHeader('Content-type', 'application/json')
-        res.write(JSON.stringify({message: "Route not found"}))
-        res.end()
-    }
+const server = createServer((req, res) => {
+    logger(req, res, () =>  {
+        if (req.url === '/api/users' && req.method === "GET") {
+            res.setHeader('Content-type', 'application/json')
+            res.write(JSON.stringify(users))
+            res.end()
+        } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+            const id = req.url.split('/')[3]
+            const user = users.find((user) => user.id === parseInt(id));
+            
+            if (user) {
+                res.setHeader('Content-type', 'application/json')
+                res.write(JSON.stringify(user))
+                res.end()
+            } else {
+                res.setHeader('Content-type', 'application/json')
+                res.statusCode = 404
+                res.write(JSON.stringify({message: "user not found"}))
+                res.end()
+            }
+    
+        } else {
+            res.setHeader('Content-type', 'application/json')
+            res.write(JSON.stringify({message: "Route not found"}))
+            res.end()
+        }
+    })
 })
 
 server.listen(PORT, () => {
